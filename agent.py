8a811d3c09e54c2e9ee468932e72d7443a2599bc
JohnRoyale <jasperkrebbers@msn.com>
2018-05-18 11:52:21 +0200

    pseudo code

--- agent.py
+++ agent.py
@@ -1,3 +1,41 @@
 from mesa import Agent
 
 class Person(Agent):
+	def __init__(self, i, model):
+		super().__init__(i, model, position, target)
+		self.alive = True
+		self.position = position
+		self.target = target
+		self.murderer = []
+		self.kb = kb
+
+
+	# actions
+
+	#move action
+	def move(self):
+
+
+	#optional action (kill, flee, stay)
+	def flee(self):
+
+
+	def kill(self):
+
+
+	def stay(self):
+		pass
+
+
+	def step(self):
+		if(self.alive):
+			if(murderer in room):
+				flee(random connect room)
+			elif(target in room):
+				kill([target])
+			else:
+				stay()
+
+			evaluateKB()
+			updateKB()
+			move()
--- model.py
+++ model.py
@@ -1,3 +1,23 @@
 from mesa import Model
 
+
 class ShipModel(Model):
+	"""The ship with a number of logical agents"""
+	def __init__(self, N):
+		self.num_agents = N
+		self.kripke model = []
+		self.rooms = []
+		self.corridors = () # dictionary
+
+		for i in range(self.num_agents):
+			a = Person(i, self)
+			self.schedule.add(a)
+
+		construct_kripke()
+
+	# functions
+	def construct_kripke(self):
+
+
+	def step(self):
+		
