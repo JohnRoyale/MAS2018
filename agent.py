50e221c5e00a15acf2cc93fb7261a44cf65d028e
paulozk <p.m.ozkohen@student.rug.nl>
2018-05-19 19:52:55 +0100

    Implemented a simple graph with 13 rooms; room and target initialization

--- agent.py
+++ agent.py
@@ -1,55 +1,64 @@
 from mesa import Agent
+import random
 
 class Person(Agent):
-	def __init__(self, i, model):
-		super().__init__(i, model, position, target)
-		self.alive = True
-		self.position = position
-		self.target = target
-		self.murderer = []
-		self.kb = kb
+    def __init__(self, i, model):
+        super().__init__(i, model, position, target)
+        self.alive = True
+        self.position = position
+        self.targets = targets
+        self.murderers = []
+        self.kb = kb
+        self.step()
+
+    # actions
+    def evaluateKB(self):
+        pass
 
 
-	# actions
-	def evaluateKB(self):
+    def updateKB(self):
+        pass
 
+    # move the agent to a random other room
+    def move(self):
+        corridors = model.corridors[position]
+        selected = random.randint(0, len(corridors) - 1)
+        self.position = selected
 
-	def updateKB(self):
+    # optional action (kill, flee, stay)
+    def flee(self):
+        self.move()
+    
+    def kill(self, target):
+        # victim agent is now no longer alive; remove him from murderer's target list
+        target.alive = False
+        self.targets.remove(target)
+        # add observers to new targets; add agent as murderer to observers;
+        #don't add self as murderer
+        for agent in room:
+            targets.add(agent)
+            if(agent != self):
+                agent.murderer.add(self)
 
+    # don't take any action
+    def stay(self):
+        pass
 
+    def step(self):
+        if(self.alive):
+            # get the room that the agent is in
+            room = model.rooms[position]
+            # if the agent is in the same room with any of its murderers, the agent flees
+            if(any(murderer in room for murderer in murderers)):
+                self.flee()
+            #if the agent is in the same room with any of its targets, the agent will select
+            # one of them randomly to kill
+            elif(any(target in room for target in targets)):
+                selected = random.randint(0, len(targets) - 1)
+                self.kill(self.targets[selected])
+            else:
+                self.stay()
 
-	# move action
-	def move(self):
-		corridors = model.corridors[position]
-		selected = random.int(len(corridors))
-		self.position = selected
-
-
-	# optional action (kill, flee, stay)
-	def flee(self):
-		self.move()
-
-	def kill(self, target):
-		victim = target[0];
-		victim.alive = False
-		for agent in room:
-			target.add(agent)
-			agent.murderer.add(self)
-
-	def stay(self):
-		pass
-
-
-
-	def step(self):
-		if(self.alive):
-			if(murderer in room):
-				self.flee(random connect room)
-			elif(target in room):
-				self.kill(self.target)
-			else:
-				self.stay()
-
-			self.evaluateKB()
-			self.updateKB()
-			
+            self.evaluateKB()
+            self.updateKB()
+            
--- model.py
+++ model.py
@@ -1,37 +1,88 @@
 from mesa import Model
 from mesa.time import RandomActivation
 from agent import Person
+import random
 
 
 class ShipModel(Model):
-	"""The ship with a number of logical agents"""
-	def __init__(self, N):
-		self.schedule = RandomActivation(self)
-		self.num_agents = N
-		self.kripke model = []
-		self.rooms = []
-		self.corridors = () # dictionary
+    """The ship with a number of logical agents"""
+    def __init__(self, N):
+        self.schedule = RandomActivation(self)
+        self.num_agents = N
+        self.kripke_model = []
+        # the rooms store which agents are currently in which rooms
+        self.rooms = []
+        # the corridors determine the connections between the rooms
+        self.corridors = {}
 
-		for i in range(self.num_agents):
-			a = Person(i, self)
-			self.schedule.add(a)
+        for i in range(self.num_agents):
+            a = Person(i, self)
+            self.schedule.add(a)
 
-		construct_kripke()
+        construct_graph()
+        init_game()
+        construct_kripke()
+        
 
-	# kripke model functions
-	def construct_kripke(self):
-
-
-
-	# all agents take a move step
-	def move_agents(self):
-		for agent in self.schedule.agent:
-			agent.move()
+    # kripke model functions
+    def construct_kripke(self):
+        pass
 
 
-	def step(self):
-		# agents perform action step
-		self.schedule.step()
+    # construct the graph, containing the rooms and their connections
+    def construct_graph(self):
+        N_rooms = 13
+        
+        # create N amount of rooms
+        for i in range(N_rooms):
+            self.rooms.append([])
+        
+        # establish the room connections
+        self.corridors[1] = [3]
+        self.corridors[2] = [3, 5]
+        self.corridors[3] = [1, 2, 4, 6]
+        self.corridors[4] = [3, 7]
+        self.corridors[5] = [2, 6, 8, 12]
+        self.corridors[6] = [3, 5, 7, 9]
+        self.corridors[7] = [4, 6, 10, 13] 
+        self.corridors[8] = [5, 9]
+        self.corridors[9] = [6, 8, 10, 11]
+        self.corridors[10] = [7, 9]
+        self.corridors[11] = [9]
+        self.corridors[12] = [5]
+        self.corridors[13] = [7]          
+       
+    # initialize rooms and targets
+    def init_game(self):
+        # randomly distribute the agents over the rooms; don't make agent spawn in the same room
+        available_rooms = [i+1 for i in range(len(self.rooms))]
+        for i in range(self.num_agents):
+            selected = random.choice(available_rooms)
+            self.rooms[selected].append(self.schedule.agent[selected])
+            available_rooms.remove(selected)
+        
+        # randomly distribute killing targets among the agents
+        available_targets = [agent for agent in self.schedule.agent]
+        for i in range(self.num_agents):
+            selected = random.choice(available_targets)
+            # an agent can't have himself as a target
+            while(self.schedule.agent[selected] != self.schedule.agent[i]):
+                selected = random.choice(available_targets)
+            self.schedule.agent[i].targets.append(self.schedule.agent[selected])
+            available_targets.remove(self.schedule.agent[selected])
+   
+            
+            
+        
 
-		# agents perform move step
-		self.move_agents()
+    # all agents take a move step
+    def move_agents(self):
+        for agent in self.schedule.agent:
+            agent.move()
+
+
+    def step(self):
+        # agents perform action step
+        self.schedule.step()
+        # agents perform move step
+        self.move_agents()
